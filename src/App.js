c9b52c62655df4132b079ef8b346c6d4ef0ed470
oscarquell <oscarquell96@gmail.com>
2024-04-28 13:21:52 +0600

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from "axios";
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
 
-function App() {
+
+const App = () => {
+  const [API, setAPI] = useState('');
+  const [apiResult, setApiResult] = useState(null);
+  const [apiErrors, setApiErrors] = useState(null);
+
+  const checkAPI = async () => {
+    try {
+      const response = await axios.get(`${API}`);
+      setApiResult(response);
+      setApiErrors(null);
+    } catch (e) {
+      setApiResult(null);
+      setApiErrors(e);
+      console.log(e)
+    }
+  };
+
+  const jsonTheme = {
+    background: 'red'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Проверка API на CORS</h1>
+      <div className='APIS'>
+        <input className='input-text' type="text" placeholder='Введите Ваш API' onChange={(e) => setAPI(e.target.value)}/>
+        <input className='input-button' type="button" value='Проверить' onClick={checkAPI}/>
+      </div>
+      <div className="API-result">
+          <div>
+            {apiResult && <JSONPretty  theme = { jsonTheme } id="json-pretty" data={apiResult}></JSONPretty>}
+          </div>
+          <div>
+            {apiErrors && <JSONPretty style={{fontSize: "1.1em", overflow: 'hidden'}} id="json-pretty" data={apiErrors}></JSONPretty>}
+          </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
